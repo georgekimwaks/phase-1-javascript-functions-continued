a071f81ad0a8eca724a5417be91d5654ed398a54
George <kimwaksgeorge1@gmail.com>
2023-09-20 16:55:23 +0300

    Task Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate'){
+    let saturday 
+    return saturday = `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office"){
+    let monday;
+
+    return monday = `This Monday, I will ${activity}.`
+}
+mondayWork('work from home');
+
+function wrapAdjective (visual = "*"){
+    return function (adjective = "special"){
+        return `You are ${visual}${adjective}${visual}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
